f26e8fe9c5afa9b8f31bb20083c98494fc22753b
mois3222 <moisky222@gmail.com>
2023-01-18 11:04:07 -0500

    commit: problema #8 solucionado

--- files/logica.js
+++ files/logica.js
@@ -102,3 +102,22 @@ const palindromeWord = (word = "") => {
 palindromeWord("amor a roma");
 
 /*8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
+
+const fillter = (arr, b) => {
+  const exp = new RegExp(b, "g"),
+    [res] = [...arr.match(exp)];
+
+  arr.split(",").forEach((element) => {
+    if (element.split("", res.length) !== "") {
+      const back = element
+        .trim()
+        .split("")
+        .splice(res.length, res.length + Infinity)
+        .reduce((acc, value) => acc + value, "");
+
+      console.log(back);
+    }
+  });
+};
+
+fillter("Moises1, Moises2, Moises3, Moises4, Moises5, ", "Moises");
